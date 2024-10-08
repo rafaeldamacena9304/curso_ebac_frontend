4d2e22a51cd9fa580d38d5873104adf2ae1baa25
rafaeldamacena9304 <158775319+rafaeldamacena9304@users.noreply.github.com>
2024-10-08 11:50:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicar(numeroA, numeroB) {
+    const resultado = numeroA * numeroB;
+    return resultado;
+}
+function saudacao(nome) {
+    return `Olá, ${nome}, como vai?`;
+}
+console.log(multiplicar(10, 20));
+console.log(saudacao('Rafael'));
---
+++ main.ts
@@ -0,0 +1,13 @@
+function multiplicar(numeroA:number, numeroB:number): number{
+    const resultado = numeroA * numeroB
+    return resultado
+}
+
+function saudacao(nome:string): string{
+    return `Olá, ${nome}, como vai?`
+}
+
+
+
+console.log(multiplicar(10,20))
+console.log(saudacao('Rafael'))
